744a0f45308b980ae5d303d11103e45c43b55154
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-15 12:35:45 +0000

    Sun, Mar 15, 2026, 8:35 PM +08:00

--- 课程实验/lec2/C.cpp
+++ 课程实验/lec2/C.cpp
@@ -2,33 +2,27 @@
 #include <vector>
 using namespace std;
 
-void removeStone(vector<int> v, int M) {
-    int minlen = INT_MAX;
-    for (int i = 0; i < v.size() - 1; i++) {
-        if (v[i] + v[i+1] < minlen) {
-            minlen = v[i] + v[i+1];
-        }
-    }
-}
 
 int main() {
     int L, M ,N;
-    cin >> L >> M >> N;
+    cin >> L >> N >> M;
     vector<int> d;
     int in;
     for (int i = 0; i < N; i++) {
         cin >> in;
         d.push_back(in);
     }
-    vector<int> nd; //空隙
-    nd.push_back(d[0]);
-    for (int i = 1; i <= N ; i++) 
-    {
-        nd.push_back(d[i] - d[i-1]);
-    }
-    nd.push_back(L-d[d.size()-1]);
 
-    for (int i = 0; i < M; i++) {
-        removeStone(nd, M);
-    }
+    /*
+    二分一个“最短跳跃距离” x：
+
+    判断是否能通过删除不超过 M 块石头，让所有相邻跳跃距离都 >= x
+
+    如果可以，说明 x 还能更大
+
+    如果不行，说明 x 太大了
+    */
+
+    
+    
 }
