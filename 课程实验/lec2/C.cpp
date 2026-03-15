0cab09b22f03068fa627e5f982c4f4e9ef71a0fa
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-15 12:43:02 +0000

    Sun, Mar 15, 2026, 8:43 PM +08:00

--- 课程实验/lec2/C.cpp
+++ 课程实验/lec2/C.cpp
@@ -25,5 +25,32 @@ int main() {
 
     // x 的范围是 [1, L]
     int left = 1, right = L;
-    
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // 判断是否能通过删除不超过 M 块石头，让所有相邻跳跃距离都 >= mid
+        int last = 0; // 上一个石头的位置
+        int removed = 0; // 已经删除的石头数量
+        for (int i = 0; i < N; i++) {
+            if (d[i] - last < mid) {
+                // 跳跃距离太小，需要删除这个石头
+                removed++;
+            } else {
+                // 跳跃距离足够，更新上一个石头的位置
+                last = d[i];
+            }
+        }
+        // 最后还要考虑从最后一个石头跳到终点的距离
+        if (L - last < mid) {
+            removed++;
+        }
+
+        if (removed <= M) {
+            // 可以通过删除不超过 M 块石头让所有相邻跳跃距离都 >= mid
+            left = mid + 1; // x 还能更大
+        } else {
+            right = mid; // x 太大了
+        }
+    }
+    cout << left - 1 << endl; // 输出最大的最短跳跃距离
 }
