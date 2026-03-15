250ac5efa09738d180e34e142342750cc3f9d523
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-15 13:17:52 +0000

    Sun, Mar 15, 2026, 9:17 PM +08:00

---
+++ 课程实验/lec2/A.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> city(N);
+    long long total = 0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> city[i].first >> city[i].second; // x, p
+        total += city[i].second;
+    }
+
+    sort(city.begin(), city.end());
+
+    long long prefix = 0;
+    double ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        prefix += city[i].second;
+        if (prefix * 2 >= total) {
+            ans = city[i].first;
+            break;
+        }
+    }
+
+    cout << fixed << setprecision(5) << ans << endl;
+    return 0;
+}
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     //电视台x的范围，在最左与最右城市之间
+//     int N;
+//     cin >> N;
+//     map<int, int> m;  // 坐标：人口
+//     int a, b;
+//     for (int i =0;i<N;i++){
+//         cin >> a >> b;
+//         m[a] = b;
+//     }
+//     int minl = INT_MAX, maxl = INT_MIN;
+//     for (const auto& [l, p] : m) {
+//         if (l > maxl) maxl = l;
+//         if (l < minl) minl = l;
+//     }
+
+//     // x 在minl和maxl之间
+//     int left = minl, right = maxl;
+//     float vmax = 0;
+//     float vx;
+//     for () {
+
+//         int vv = 0;
+//         float x = left + (right - left) / 2;
+    
+//         int vv = 0;
+//         for (const auto& [l, p] : m) {
+//             vv += abs(l-x) * p;
+//         }
+
+//         if (vv > vmin) {
+//             vx = x;
+//             vmax = vv;
+//         }
+//     }
+// }
