697dfbff251c0ae6de913b89b67f82c7500d84fa
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-18 14:12:43 +0000

    Wed, Mar 18, 2026, 10:12 PM +08:00

---
+++ 课程实验/lec2/D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int k, s;
+    vector<pair<int, int> > candidates;
+    for (int i = 0; i < n; i++) {
+        candidates.push_back(make_pair(k, s));
+    }
+}
