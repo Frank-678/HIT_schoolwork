f9131523c4c3d04e509c8cd99231027590f7e74c
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-18 14:18:07 +0000

    Wed, Mar 18, 2026, 10:18 PM +08:00

--- 课程实验/lec2/D.cpp
+++ 课程实验/lec2/D.cpp
@@ -4,6 +4,10 @@
 #include <iomanip>
 using namespace std;
 
+bool cmp (pair<int, int>& a, pair<int, int>& b) {
+    return a.second > b.second;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
@@ -12,4 +16,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         candidates.push_back(make_pair(k, s));
     }
+    sort(candidates.begin(), candidates.end(), cmp);
+
+    int last_person = round(m * 1.5 + 0.5);
+    
 }
