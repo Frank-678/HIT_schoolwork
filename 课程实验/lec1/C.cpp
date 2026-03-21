f1d71fa088c476c094382a12c3b01e84b147680f
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-21 09:02:38 +0000

    Sat, Mar 21, 2026, 5:02 PM +08:00

--- 课程实验/lec1/C.cpp
+++ 课程实验/lec1/C.cpp
@@ -13,6 +13,28 @@ int main() {
         v.push_back(s);
     }
 
+    
+
+    return 0;
+}
+
+
+/* Time Limit EXCEEDED
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> n >> m;
+    string s;
+    vector<string> v;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        v.push_back(s);
+    }
+
     bool can_delete = false;
     bool can_on;
     for (int i = 0; i < n; i++) { // 枚举删哪一行
@@ -51,38 +73,4 @@ int main() {
 
     return 0;
 }
-
-// #include <iostream>
-// #include <vector>
-// #include <cstdlib>
-// #include <string>
-// using namespace std;
-
-// int main() {
-//     int m, n;
-//     cin >> m >> n;
-//     string s;
-//     vector<string> v;
-//     for (int i = 0; i < m; i++) {
-//         cin >> s;
-//         v.push_back(s);
-//     }
-//     for (int j = 0; j < v.size(); j++) { //string
-//         for (int i = 0; i < n; i++) { //char
-//             if (v[j][i] == '1') {
-//                 int hasLight = false;
-//                 for (int k = 0; k < v.size(); k++) { //string
-//                     if (k != j && v[k][i] == '1') {
-//                         hasLight = true;
-//                     }
-//                 }
-//                 if (!hasLight) {
-//                     cout << "NO" << flush;
-//                     exit(0);
-//                 }
-//             }
-//         }
-//     }
-//     cout << "YES" << flush;
-//     return 0;
-// }
+*/
